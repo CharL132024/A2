1b8a52ac643f1e132fe9ba21e4368a42f38abba9
Charlotte Balcomb <charlotte@Charlottes-MacBook-Air.local>
2024-09-16 09:05:26 +1000

    final

--- src/Components/Main/Tickets.js
+++ src/Components/Main/Tickets.js
@@ -5,7 +5,7 @@ const Tickets = () => {
     return (
         <div className="Tickets" style={{backgroundColor:"rgba(250, 196, 204, 0.504)", color:"black", border:"2px solid black"}}>
             <h2>Tickets</h2>
-            <h3> Tickets for our 2024 home games are now available via Ticketmaster. View our full fixture </h3>
+            <h3> Tickets for our 2024 home games are now available via Ticketmaster. View our full fixture <a href="Newcastle Nighthawks 2024 Fixture_.pdf">HERE</a></h3>
             <br/>
             <h3> Members are able to select the games they would like to attend from Wednesday 6 March. Please note the club has placed a large number of tickets on hold for members which will be ready for selection from this date.</h3>
             <br/>
